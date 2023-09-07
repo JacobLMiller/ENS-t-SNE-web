cfae5eab078b8c4751fffac2a0318b6798a63d09
Jacob Lee Miller <jacobmiller1@cs.arizona.edu>
2023-09-07 16:45:02 -0700

    initial commit

--- src/application/routes.py
+++ src/application/routes.py
@@ -2,10 +2,14 @@ from flask import jsonify, render_template, request
 from application import app
 from werkzeug.utils import secure_filename
 
+import json 
+with open("src/application/static/data/penguins.json", "r") as fdata:
+    data = json.load(fdata)
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='template', data=None)
+    return render_template('index.html', title='template', data=data)
 
 
 @app.route('/about')
